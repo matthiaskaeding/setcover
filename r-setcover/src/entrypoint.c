28173edba31c782cf4a5a495c6a002f0490b4588
matthiaskaeding <kaedingmatthias@gmail.com>
2025-12-17 18:31:18 +0100

    Reset some changes

--- r-setcover/src/entrypoint.c
+++
@@ -1,9 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-#include <R_ext/Rdynload.h>
-
-void R_init_rsetcover_extendr(DllInfo *dll);
-
-void R_init_rsetcover(DllInfo *dll) {
-    R_init_rsetcover_extendr(dll);
-}
